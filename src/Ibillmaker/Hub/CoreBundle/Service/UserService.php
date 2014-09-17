88c1082dd5562dde48a5643da1e183e3edc46c0d
ngpatel143 <ngpatel@outlook.com>
2014-09-18 00:32:39 +0200

    Test Fake - new user interface.

---
+++ src/Ibillmaker/Hub/CoreBundle/Service/UserService.php
@@ -0,0 +1,14 @@
+<?php
+namespace Ibillmaker\Hub\CoreBundle\Service;
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ * Auther Nishant Patel
+ */
+Class UserService
+{
+    
+}
+
